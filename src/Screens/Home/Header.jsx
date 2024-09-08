3de84f397a54256ec1f3a32d8cad532d53b36070
AnasBilal26 <anasbilalbhai@gmail.com>
2024-09-08 17:52:23 +0500

    learn

--- src/Screens/Home/Header.jsx
+++ src/Screens/Home/Header.jsx
@@ -1,28 +1,30 @@
+/** @format */
+
 import React, { useState } from 'react';
 import { BsPersonCircle, BsJustify } from 'react-icons/bs';
 import { FaGraduationCap } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
-import { auth } from '../../Configuration/Firebase'; 
+import { auth } from '../../Configuration/Firebase';
 import styles from './Home.module.css';
-
+import Ahsanpic from '../../image/ahsan pic.png';
 function Header({ OpenSidebar }) {
-  const [showLogout, setShowLogout] = useState(false); 
+  const [showLogout, setShowLogout] = useState(false);
   const navigate = useNavigate();
 
   const handleLogout = async () => {
     // first clear the local storage after logout
     try {
-      await signOut(auth); 
-      localStorage.clear(); 
-      navigate('/'); 
+      await signOut(auth);
+      localStorage.clear();
+      navigate('/');
     } catch (error) {
       console.error('Error during logout:', error);
     }
   };
 
   const toggleLogoutButton = () => {
-    setShowLogout(!showLogout); 
+    setShowLogout(!showLogout);
   };
 
   return (
@@ -36,8 +38,8 @@ function Header({ OpenSidebar }) {
       </div>
       <div className={styles.headerright}>
         <div className={styles.personContainer} onClick={toggleLogoutButton}>
-          <BsPersonCircle className={styles.iconheader} />
-          {showLogout && (  
+          <img className={styles.iconheader} src={Ahsanpic} alt="" />
+          {showLogout && (
             <button className={styles.logoutButton} onClick={handleLogout}>
               Logout
             </button>
